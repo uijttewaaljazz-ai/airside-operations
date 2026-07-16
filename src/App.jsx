dd1469f9872b7f73e290d3ce367511b8c54cd2dd
Jazz Uijttewaal <jazzuijttewaal@MacBook-Air-van-Jazz.local>
2026-07-16 14:13:24 +0200

    Fix dark screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './App.css'
 import { supabase } from './services/supabase.js'
 import Header from './components/Header.jsx'
